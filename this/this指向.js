be4653abd12fa56c1449fa634adf1b1bf016fa09
邵泽林 <shaozl@cnooc.com.cn>
2019-05-13 15:20:43 +0800

    this指向问题的解释

--- this/this指向.js
+++ this/this指向.js
@@ -1,12 +1,25 @@
-let controller = {
-  _val:0,
-  makeReq: function(val) {
-    let self = this
-    self._val = val
-    console.log(self._val)
-    if(val==1)
-      self.makeReq(225)
+var num = 250
+
+function newObj() {
+ this.num = 123
+  this.consoleLog = function () {
+    console.log(this.num)
   }
 }
 
-controller.makeReq(1)
+let obj = new newObj()
+
+let testObj = {
+  num: 2222
+}
+
+obj.consoleLog() //.bind(testObj)()
+console.log(document.num)
+
+// 1. bind 和 call 其实是第一级 显式
+// 2. new 和 {} 其实是第二级 隐式
+// 3. 实际使用(根据所使用作用域，这种都是和闭包有关吧？)
+// 4. 最终，寻找到window上
+// 5. 严格模式就只能bind undefined, gg
+
+// 说白了，就是this在运行时才会决定会
